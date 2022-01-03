fb681803ec41c5cef909de4317a4f13691f244e3
Nick Forneris <nicholasdforneris@gmail.com>
2022-01-03 14:53:46 -0600

    Restructuring for data handling

--- src/Data/GetData.js
+++ src/Data/GetData.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import { useState, useEffect } from 'react'
-const KEY = process.env.REACT_APP_API_KEY;
+import { useOptionData } from "./OptionDataProvider";
 
-export const DataContext = React.createContext(null);
-export const StrikeDatesContext = React.createContext(null);
+import { useEffect } from 'react'
+const KEY = process.env.REACT_APP_API_KEY;
 
 const GetData = (ticker, expMonth) => {
 
@@ -13,13 +11,13 @@ const GetData = (ticker, expMonth) => {
     const day = new Date(year, month, 0).toLocaleDateString(`en`, { day: `2-digit` })
     const request = `https://api.tdameritrade.com/v1/marketdata/chains?apikey=${KEY}&symbol=${ticker}&fromDate=${year}-${month}-01&toDate=${year}-${month}-${day}`;
 
-    const [data, setData] = useState([null])
+    console.log('API CALL: ', request)
 
     const fetchData = () => {
         fetch(request)
             .then((response) => response.json())
             .then((data) => {
-                setData(data)
+                console.log('FETCHED OPTION DATA: ', data)
             })
     }
 
@@ -27,11 +25,6 @@ const GetData = (ticker, expMonth) => {
     useEffect(() => {
         fetchData()
     }, [ticker, expMonth])
-
-
-    console.log({'From GetData': data.underlyingPrice})
-    
-    return {data}
 }
 
 export default GetData
