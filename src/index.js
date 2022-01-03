a2348ff8bfbcc7454d0333a2d0ffdf23d46d5afd
Nick Forneris <nicholasdforneris@gmail.com>
2022-01-03 14:53:01 -0600

    Restructuring for data handling

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { OptionDataProvider } from './Data/OptionDataProvider'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <OptionDataProvider>
     <App />
+    </OptionDataProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
