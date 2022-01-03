271ff4309c25c0b6964e9c19de1a58f3d1111f73
Nick Forneris <nicholasdforneris@gmail.com>
2022-01-03 14:52:05 -0600

    Restructuring for data handling

--- src/App.js
+++ src/App.js
@@ -1,74 +1,11 @@
-import React, { useState, useRef } from 'react';
-import { DropdownButton, Dropdown, Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+import Nav from './Components/Nav'
 import Cards from './Components/Cards';
 
-export const TickerContext = React.createContext(null);
-export const ExpMonthContext = React.createContext(null);
-
 const App = () => {
-  const tickerInput = useRef();
-  const [ticker, setTicker] = useState(null)
-  const [expMonth, setExpMonth] = useState('CHOOSE EXP MONTH')
-
-  /* Set ticker and expiration month after expiration month is chosen*/
-  const handleChange = (event) => {
-    setExpMonth(event)
-    setTicker(tickerInput.current.value.toUpperCase())
-  }
-  
-  const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
-
   return (
     <>
-    <ExpMonthContext.Provider value={expMonth}>
-      <TickerContext.Provider value={ticker}>
-        <Navbar expand="lg" bg="dark" variant="dark" sticky="top">
-          {/*<Navbar.Brand href="#home">
-            <img src={process.env.PUBLIC_URL + '/OptionsOptimizer.png'} width='150' />
-          </Navbar.Brand>*/}
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className='ml-0'>
-              <Form className="d-flex">
-                <FormControl
-                  type="text"
-                  id="ticker"
-                  className="m-2"
-                  name="ticker"
-                  ref={tickerInput}
-                  placeholder="TICKER"
-                  aria-label="TICKER"
-                  required
-                />
-              </Form>
-              {/*Dropdown values used for EXP date selection - sets state*/}
-              <DropdownButton
-                className="m-2"
-                variant="outline-light"
-                title={expMonth}
-                id="collasible-nav-dropdown"
-                onSelect={handleChange}
-              >
-                {/*Expiration by month choices*/}
-                {months.map((month) => 
-                <Dropdown.Item
-                  key={month}
-                  id="expMonth"
-                  eventKey={month}
-                >
-                  {month}
-                </Dropdown.Item>
-                )}
-              </DropdownButton>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-
-        {/* Display Cards */}
-      <Cards/>
-
-      </TickerContext.Provider>
-      </ExpMonthContext.Provider>
+      <Nav />
+      <Cards />
     </>
   );
 }
