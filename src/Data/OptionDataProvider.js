a8529f5d634fbae7e170d5e8dc35dd2164debd72
Nick Forneris <nicholasdforneris@gmail.com>
2022-01-03 14:54:37 -0600

    Restructuring for data handling

---
+++ src/Data/OptionDataProvider.js
@@ -0,0 +1,22 @@
+import React, {useState, createContext, useContext} from 'react'
+
+const OptionDataContext = React.createContext()
+
+const OptionDataProvider = ({ children }) => {
+
+    const [optionData, setOptionData] = useState([null])
+
+    return (
+        <OptionDataContext.Provider value={optionData, setOptionData}>{children}</OptionDataContext.Provider>
+    )
+}
+
+const useOptionData = () => {
+    const context = React.useContext(OptionDataContext)
+    if (context === undefined) {
+        throw new Error('useOptionData must be used within an OptionDataProvider')
+    }
+    return context
+}
+
+export { OptionDataProvider, useOptionData }
