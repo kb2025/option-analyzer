0896ed687dbec8d135cacbc4d06244db7cf7a7de
Nick Forneris <nicholasdforneris@gmail.com>
2022-01-06 15:07:50 -0600

    Removed date selection buttons
    Will separate other inputs into components

--- src/Components/Inputs.js
+++ src/Components/Inputs.js
@@ -2,35 +2,25 @@ import React, { useState, useRef } from 'react';
 import { DropdownButton, Dropdown, Form, FormControl, ButtonGroup, ToggleButton } from 'react-bootstrap';
 import UseGetData from '../DataHandlers/GetData'
 import useTransformData from '../DataHandlers/TransformData';
+import DateSelectButtons from './DateSelectButtons'
 
 const TickerExpInputs = () => {
-    const [radioValue, setRadioValue] = useState(null);
-   /* const { currentPrice, uniqueDates } = useTransformData(radioValue) */
+    const { currentPrice } = useTransformData()
+    let underlyinPrice = (currentPrice) ? parseFloat(underlyinPrice).toFixed(2) : '$0.00'
 
     useTransformData()
     
     const tickerInput = useRef();
-
     const [ticker, setTicker] = useState(null)
     const [expMonth, setExpMonth] = useState('CHOOSE EXP MONTH')
-
     const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
 
-    UseGetData(ticker, expMonth, radioValue)
+    UseGetData(ticker, expMonth)
 
     /* Set ticker and expiration month after expiration month is chosen*/
     const handleChange = (event) => {
         setExpMonth(event)
         setTicker(tickerInput.current.value.toUpperCase())
-    }
-
-    const tickerCheck = () => {
-    /*if (ticker) {
-        return (
-        <div className='text-white text-center ml-2 mr-2'><strong>Current Price</strong><br></br>${currentPrice}</div>
-        )} else {
-        <div className='text-white text-center ml-2 mr-2'><strong>Enter Ticker Symbol</strong></div>
-        }*/
     }
 
     return (
@@ -48,7 +38,11 @@ const TickerExpInputs = () => {
                 />
             </Form>
             
-            {tickerCheck()}
+            <div className='text-white text-center ml-2 mr-2'>
+                <strong>Current Price</strong>
+                <br></br>
+                {underlyinPrice}
+                </div>
 
             {/*Dropdown values used for EXP date selection - sets state*/}
             <DropdownButton
@@ -68,25 +62,8 @@ const TickerExpInputs = () => {
                         {month}
                     </Dropdown.Item>
                 )}
-            </DropdownButton>
-                    
-         {/*}   <ButtonGroup className="m-2">
-          {uniqueDates.map((radio, id) => (
-            <ToggleButton
-              key={id}
-              id={`radio-${id}`}
-              type="radio"
-              variant="outline-light"
-              name={radio}
-              value={radio}
-              checked={radioValue === radio}
-              onChange={(e) => setRadioValue(e.currentTarget.value)}
-            >
-              {radio}
-            </ToggleButton>
-          ))}
-        </ButtonGroup>*/}
-            
+            </DropdownButton>   
+            <DateSelectButtons />         
         </>
     );
 }
