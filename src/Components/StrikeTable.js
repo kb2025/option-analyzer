5e4b208359ed7800f01ffbb93950ec204a2bfad4
Nick Forneris <nicholasdforneris@gmail.com>
2022-01-11 15:34:21 -0600

    Stacking results on top of strike table

--- src/Components/StrikeTable.js
+++ src/Components/StrikeTable.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import useTransformData from '../DataHandlers/useTransformData'
+import ResultsCard from './ResultsCard'
 import { useSelectedDate } from '../Providers/SelectedDateProvider'
 import { useResultsData } from '../Providers/ResultsDataProvider'
 
@@ -19,39 +20,38 @@ const StrikeTable = () => {
     const strikes = putStrikes
 
     if (expDates) {
-        selectedDate = (selectedDate) ? selectedDate : expDates[expDates.length-1]
+        selectedDate = (selectedDate) ? selectedDate : expDates[expDates.length - 1]
         return (
             <div className='card scroll'>
+                <ResultsCard />
                 {expDates.map((date, i) => {
                     if (date == selectedDate) {
                         return (
                             <>
                                 <div className='card-header bg-dark text-white text-center'>
                                     {date.replace(":", ` | Days Until Expiration `)}
+                                       <br></br>Current Price: ${parseFloat(underlyingPrice).toFixed(2)}
+                                        <div className='row'>
+                                            <div className="col text-center bg-danger m-0">
+                                                PUT
+                                            </div>
+                                            <div className="col text-center bg-success m-0">
+                                                CALL
+                                        </div>
+                                    </div>
                                 </div>
                                 <div id="table" className="table-responsive m-0 p-0">
                                     <table className="table table-sm table-striped table-dark m-0 p-0">
-                                        <thead>
-                                            <tr>
-                                                <th colSpan="4" className="text-center bg-danger">
-                                                    PUT
-                                                </th>
-                                                <th className="text-center">
-                                                    CURRENT PRICE:   ${parseFloat(underlyingPrice).toFixed(2)}
-                                                </th>
-                                                <th colSpan="4" className="text-center bg-success">
-                                                    CALL
-                                                </th>
-                                            </tr>
+                                        <thead className='header p-5'>
                                             <tr>
                                                 <th className="text-center">+/-</th>
-                                                <th className="text-center">OPEN INTEREST</th>
+                                                <th className="text-center">OI</th>
                                                 <th className="text-center">DELTA</th>
                                                 <th className="text-center">MARK</th>
                                                 <th className="text-center">STRIKE</th>
                                                 <th className="text-center">MARK</th>
                                                 <th className="text-center">DELTA</th>
-                                                <th className="text-center">OPEN INTEREST</th>
+                                                <th className="text-center">OI</th>
                                                 <th className="text-center">+/-</th>
                                             </tr>
                                         </thead>
@@ -63,27 +63,27 @@ const StrikeTable = () => {
                                                             <button
                                                                 className='btn btn-success btn-sm m-1'
                                                                 value={'PUT'}
-                                                                onClick={() => setResultsData([...resultsData, {
+                                                                onClick={() => (resultsData.length < 4) ? setResultsData([...resultsData, {
                                                                     PUT:
                                                                         [
                                                                             parseFloat(strike),
                                                                             parseFloat(-putStrikes[i][strike][0].mark.toFixed(2)),
                                                                             'BUY'
                                                                         ]
-                                                                }])}>
+                                                                }]) : null}>
                                                                 BUY
                                                             </button>
                                                             <button
                                                                 className='btn btn-danger btn-sm m-1'
                                                                 value={'PUT'}
-                                                                onClick={() => setResultsData([...resultsData, {
+                                                                onClick={() => (resultsData.length < 4) ? setResultsData([...resultsData, {
                                                                     PUT:
                                                                         [
                                                                             parseFloat(strike),
                                                                             parseFloat(putStrikes[i][strike][0].mark.toFixed(2)),
                                                                             'SELL'
                                                                         ]
-                                                                }])}>
+                                                                }]) : null}>
                                                                 SELL
                                                             </button>
                                                         </td>
@@ -91,7 +91,7 @@ const StrikeTable = () => {
                                                             {putStrikes[i][strike][0].openInterest}
                                                         </td>
                                                         <td className="text-center">
-                                                            {putStrikes[i][strike][0].delta}
+                                                            {parseFloat(putStrikes[i][strike][0].delta).toFixed(2)}
                                                         </td>
                                                         <td className="text-center">
                                                             ${putStrikes[i][strike][0].mark}
@@ -103,7 +103,7 @@ const StrikeTable = () => {
                                                             ${callStrikes[i][strike][0].mark}
                                                         </td>
                                                         <td className="text-center">
-                                                            {callStrikes[i][strike][0].delta}
+                                                            {parseFloat(callStrikes[i][strike][0].delta).toFixed(2)}
                                                         </td>
                                                         <td className="text-center">
                                                             {callStrikes[i][strike][0].openInterest}
@@ -111,26 +111,26 @@ const StrikeTable = () => {
                                                         <td className="text-center">
                                                             <button
                                                                 className='btn btn-success btn-sm m-1'
-                                                                onClick={() => setResultsData([...resultsData, {
+                                                                onClick={() => (resultsData.length < 4) ? setResultsData([...resultsData, {
                                                                     CALL:
                                                                         [
                                                                             parseFloat(strike),
                                                                             parseFloat(-callStrikes[i][strike][0].mark.toFixed(2)),
                                                                             'BUY'
                                                                         ]
-                                                                }])}>
+                                                                }]) : null}>
                                                                 BUY
                                                             </button>
                                                             <button
                                                                 className='btn btn-danger btn-sm m-1'
-                                                                onClick={() => setResultsData([...resultsData, {
+                                                                onClick={() => (resultsData.length < 4) ? setResultsData([...resultsData, {
                                                                     CALL:
                                                                         [
                                                                             parseFloat(strike),
                                                                             parseFloat(callStrikes[i][strike][0].mark.toFixed(2)),
                                                                             'SELL'
                                                                         ]
-                                                                }])}>
+                                                                }]) : null}>
                                                                 SELL
                                                             </button>
                                                         </td>
