f62e437748804ae5b6cfa22a095603c62ce074c2
Nick Forneris <nicholasdforneris@gmail.com>
2022-01-12 17:00:52 -0600

    Layout Adjustment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import Nav from './Components/Nav'
 import StrikeTable from './Components/StrikeTable';
-import ResultsCard from './Components/ResultsCard';
-import { Container, Row, Col } from 'react-bootstrap';
 
 const App = () => {
   return (
     <>
-      <Container fluid>
         <Nav />
         <StrikeTable />
-      </Container>
     </>
   );
 }
