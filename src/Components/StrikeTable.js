715cfc70cbabdf5b6dd4f6b1d9db2a54c93167a6
Nick Forneris <nicholasdforneris@gmail.com>
2022-01-12 16:09:04 -0600

    Layout Adjustment

--- src/Components/StrikeTable.js
+++ src/Components/StrikeTable.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import useTransformData from '../DataHandlers/useTransformData'
-import ResultsCard from './ResultsCard'
 import { useSelectedDate } from '../Providers/SelectedDateProvider'
 import { useResultsData } from '../Providers/ResultsDataProvider'
+import StrikeSelections from './StrikeSelections'
 
 const StrikeTable = () => {
 
@@ -10,7 +10,6 @@ const StrikeTable = () => {
     const { resultsData, setResultsData } = useResultsData()
 
     const {
-        optionData,
         underlyingPrice,
         expDates,
         callStrikes,
@@ -22,28 +21,36 @@ const StrikeTable = () => {
     if (expDates) {
         selectedDate = (selectedDate) ? selectedDate : expDates[expDates.length - 1]
         return (
-            <div className='card scroll'>
-                <ResultsCard />
+            <div className='bg-dark'>
                 {expDates.map((date, i) => {
-                    if (date == selectedDate) {
+                    if (date === selectedDate) {
                         return (
                             <>
-                                <div className='card-header bg-dark text-white text-center'>
-                                    {date.replace(":", ` | Days Until Expiration `)}
-                                       <br></br>Current Price: ${parseFloat(underlyingPrice).toFixed(2)}
-                                        <div className='row'>
-                                            <div className="col text-center bg-danger m-0">
-                                                PUT
-                                            </div>
-                                            <div className="col text-center bg-success m-0">
-                                                CALL
-                                        </div>
-                                    </div>
-                                </div>
-                                <div id="table" className="table-responsive m-0 p-0">
-                                    <table className="table table-sm table-striped table-dark m-0 p-0">
-                                        <thead className='header p-5'>
+                                <div id="table" className="table-responsive scroll">
+                                    <table className="table table-sm table-striped table-dark">
+                                        <thead className='header'>
                                             <tr>
+                                                <th colspan="9" className="text-center p-0">
+                                                Strike Selections
+                                                </th>
+                                            </tr>
+                                            <tr className='sticky-row'>
+                                                <th colspan="9" className="text-center p-0">
+                                            <div className='row justify-content-center p-0'><StrikeSelections/></div>
+                                                </th>
+                                            </tr>
+                                            <tr className='sticky-row'>
+                                                <th  colspan="9" className="text-center p-0">
+                                                {date.replace(":", ` | Days Until Expiration: `)}<br></br>
+                                                Underlying Price: ${parseFloat(underlyingPrice).toFixed(2)}    
+                                                </th>
+                                            </tr>
+                                            <tr className='sticky-row'>
+                                                <th colspan="4" className="text-center bg-danger p-0">PUT</th>
+                                                <th colspan='1'></th>
+                                                <th colspan="4" className="text-center bg-success p-0">CALL</th>
+                                            </tr>
+                                            <tr className='sticky-row'>
                                                 <th className="text-center">+/-</th>
                                                 <th className="text-center">OI</th>
                                                 <th className="text-center">DELTA</th>
