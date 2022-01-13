d0dc56f8e34ee4fa9b49a219de2425c1ad6507be
Nick Forneris <nicholasdforneris@gmail.com>
2022-01-13 15:56:48 -0600

    fixed sign errors

--- src/Components/StrikeSelections.js
+++ src/Components/StrikeSelections.js
@@ -30,7 +30,7 @@ const StrikeSelections = () => {
                         return (
                             <div key={resultsData.indexOf(item)} className='col-auto'>
                             <button
-                                className='btn btn-sm btn-danger m-1'
+                                className='btn btn-sm btn-success m-1'
                                 id={resultsData.indexOf(item)}
                                 value={item.CALL[0], item.CALL[1]}
                                 onClick={(event) => handleClick(event.target.id)}
@@ -45,7 +45,7 @@ const StrikeSelections = () => {
                         return (
                             <div key={resultsData.indexOf(item)} className='col-auto'>
                             <button
-                                className='btn btn-sm btn-success m-1'
+                                className='btn btn-sm btn-danger m-1'
                                 id={resultsData.indexOf(item)}
                                 value={item.PUT[0], item.PUT[1]}
                                 onClick={(event) => handleClick(event.target.id)}>
