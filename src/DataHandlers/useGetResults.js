4ba1482a1a4fddfcd70d72a6b6ef9a86e9d2e14c
Nick Forneris <nicholasdforneris@gmail.com>
2022-01-13 15:58:26 -0600

    Fixed profit level per strike calcs

--- src/DataHandlers/useGetResults.js
+++ src/DataHandlers/useGetResults.js
@@ -33,96 +33,118 @@ const useGetResults = () => {
         }
     }
 
+    strikes = strikes.sort()
+
     highestStrike = Math.max(...strikes)+1
 
     for (let item in resultsData) {
+        
         if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='BUY') {
-            zeroPl.push(Math.max(parseFloat(0-(resultsData[item].CALL[0])),0)+(resultsData[item].CALL[1]*100))
-        } else if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='SELL') {
-            zeroPl.push(Math.max(parseFloat(0-(resultsData[item].CALL[0])),0)+(resultsData[item].CALL[1]*100))
-        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]==='BUY'){
-            zeroPl.push(Math.max(parseFloat((resultsData[item].PUT[0])-0),0)+(resultsData[item].PUT[1]*100))
-        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]==='SELL'){
-            zeroPl.push(Math.max(parseFloat((resultsData[item].PUT[0])-0),0)+(resultsData[item].PUT[1]*100))
-        }
+            zeroPl.push((Math.max(0-parseFloat(resultsData[item].CALL[0]),0)-parseFloat(resultsData[item].CALL[1]))*100)
 
-        if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='BUY') {
-            firstLegPl.push(Math.max(parseFloat(strikes[0]-(resultsData[item].CALL[0])),0)+(resultsData[item].CALL[1]*100))
         } else if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='SELL') {
-            firstLegPl.push(Math.max(parseFloat(strikes[0]-(resultsData[item].CALL[0])),0)+(resultsData[item].CALL[1]*100))
-        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]==='BUY'){
-            firstLegPl.push(Math.max(parseFloat((resultsData[item].PUT[0])-strikes[0]),0)+(resultsData[item].PUT[1]*100))
-        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]==='SELL'){
-            firstLegPl.push(Math.max(parseFloat((resultsData[item].PUT[0])-strikes[0]),0)+(resultsData[item].PUT[1]*100))
-        }
+            zeroPl.push(-(Math.max(0-parseFloat(resultsData[item].CALL[0]),0)-parseFloat(resultsData[item].CALL[1]))*100)
 
-        if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='BUY') {
-            secondLegPl.push(Math.max(parseFloat(strikes[1]-(resultsData[item].CALL[0])),0)+(resultsData[item].CALL[1]*100))
-        } else if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='SELL') {
-            secondLegPl.push(Math.max(parseFloat(strikes[1]-(resultsData[item].CALL[0])),0)+(resultsData[item].CALL[1]*100))
-        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]==='BUY'){
-            secondLegPl.push(Math.max(parseFloat((resultsData[item].PUT[0])-strikes[1]),0)+(resultsData[item].PUT[1]*100))
-        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]==='SELL'){
-            secondLegPl.push(Math.max(parseFloat((resultsData[item].PUT[0])-strikes[1]),0)+(resultsData[item].PUT[1]*100))
-        }
+        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]=='BUY'){
+            zeroPl.push((Math.max(parseFloat(resultsData[item].PUT[0])-0,0)-parseFloat(resultsData[item].PUT[1]))*100)
 
-        if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='BUY') {
-            thirdLegPl.push(Math.max(parseFloat(strikes[2]-(resultsData[item].CALL[0])),0)+(resultsData[item].CALL[1]*100))
-        } else if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='SELL') {
-            thirdLegPl.push(Math.max(parseFloat(strikes[2]-(resultsData[item].CALL[0])),0)+(resultsData[item].CALL[1]*100))
-        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]==='BUY'){
-            thirdLegPl.push(Math.max(parseFloat((resultsData[item].PUT[0])-strikes[2]),0)+(resultsData[item].PUT[1]*100))
-        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]==='SELL'){
-            thirdLegPl.push(Math.max(parseFloat((resultsData[item].PUT[0])-strikes[2]),0)+(resultsData[item].PUT[1]*100))
-        }
-
-        if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='BUY') {
-            fourthLegPl.push(Math.max(parseFloat(strikes[3]-(resultsData[item].CALL[0])),0)+(resultsData[item].CALL[1]*100))
-        } else if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='SELL') {
-            fourthLegPl.push(Math.max(parseFloat(strikes[3]-(resultsData[item].CALL[0])),0)+(resultsData[item].CALL[1]*100))
-        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]==='BUY'){
-            fourthLegPl.push(Math.max(parseFloat((resultsData[item].PUT[0])-strikes[3]),0)+(resultsData[item].PUT[1]*100))
-        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]==='SELL'){
-            fourthLegPl.push(Math.max(parseFloat((resultsData[item].PUT[0])-strikes[3]),0)+(resultsData[item].PUT[1]*100))
-        }
-
-        if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='BUY') {
-            infiniteCheck.push(Math.max(parseFloat(highestStrike-(resultsData[item].CALL[0])),0)-(resultsData[item].CALL[1]*100))
-        } else if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='SELL') {
-            infiniteCheck.push(Math.max(parseFloat(highestStrike-(resultsData[item].CALL[0])),0)+(resultsData[item].CALL[1]*100))
-        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]==='BUY'){
-            infiniteCheck.push(Math.max(parseFloat((resultsData[item].PUT[0])-highestStrike),0)-(resultsData[item].PUT[1]*100))
-        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]==='SELL'){
-            infiniteCheck.push(Math.max(parseFloat((resultsData[item].PUT[0])-highestStrike),0)+(resultsData[item].PUT[1]*100))
-        }
-
-        totals.push(zeroPl.reduce((p,c)=>p+c),firstLegPl.reduce((p,c)=>p+c),secondLegPl.reduce((p,c)=>p+c),thirdLegPl.reduce((p,c)=>p+c),fourthLegPl.reduce((p,c)=>p+c),infiniteCheck.reduce((p,c)=>p+c))
-        totals = totals.filter((item) => item == Number(item))
-        maxProfit=Math.max(...totals)
-        maxLoss=Math.min(...totals)
-        setMaxProfit(maxProfit)
-        setMaxLoss(maxLoss)
+        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]=='SELL'){
+            zeroPl.push(-(Math.max(parseFloat(resultsData[item].PUT[0])-0,0)-parseFloat(resultsData[item].PUT[1]))*100)
+        }    
     }
 
-    console.log('Strikes:',strikes)
-    console.log('HighestStrike:',highestStrike)
-    console.log('zero PL:', zeroPl.reduce((p,c)=>p+c))
-    console.log('first leg:', firstLegPl.reduce((p,c)=>p+c))
-    console.log('second leg:',secondLegPl.reduce((p,c)=>p+c))
-    console.log('third leg:',thirdLegPl.reduce((p,c)=>p+c))
-    console.log('fourth leg:',fourthLegPl.reduce((p,c)=>p+c))
-    console.log('infiniteCheck:', infiniteCheck.reduce((p,c)=>p+c))
-console.log(totals)
-    console.log(maxProfit)
-    console.log(maxLoss)
-}    
+    for (let item in resultsData) {
+        
+        if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='BUY') {
+            firstLegPl.push((Math.max(strikes[0]-parseFloat(resultsData[item].CALL[0]),0)-parseFloat(resultsData[item].CALL[1]))*100)
+
+        } else if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='SELL') {
+            firstLegPl.push(-(Math.max(strikes[0]-parseFloat(resultsData[item].CALL[0]),0)-parseFloat(resultsData[item].CALL[1]))*100)
+
+        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]=='BUY'){
+            firstLegPl.push((Math.max(parseFloat(resultsData[item].PUT[0])-strikes[0],0)-parseFloat(resultsData[item].PUT[1]))*100)
+
+        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]=='SELL'){
+            firstLegPl.push(-(Math.max(parseFloat(resultsData[item].PUT[0])-strikes[0],0)-parseFloat(resultsData[item].PUT[1]))*100)
+        }    
     }
+
+    for (let item in resultsData) {
+        
+        if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='BUY') {
+            secondLegPl.push((Math.max(strikes[1]-parseFloat(resultsData[item].CALL[0]),0)-parseFloat(resultsData[item].CALL[1]))*100)
+
+        } else if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='SELL') {
+            secondLegPl.push(-(Math.max(strikes[1]-parseFloat(resultsData[item].CALL[0]),0)-parseFloat(resultsData[item].CALL[1]))*100)
+
+        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]=='BUY'){
+            secondLegPl.push((Math.max(parseFloat(resultsData[item].PUT[0])-strikes[1],0)-parseFloat(resultsData[item].PUT[1]))*100)
+
+        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]=='SELL'){
+            secondLegPl.push(-(Math.max(parseFloat(resultsData[item].PUT[0])-strikes[1],0)-parseFloat(resultsData[item].PUT[1]))*100)
+        }    
+    }
+
+    for (let item in resultsData) {
+        
+        if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='BUY') {
+            thirdLegPl.push((Math.max(strikes[2]-parseFloat(resultsData[item].CALL[0]),0)-parseFloat(resultsData[item].CALL[1]))*100)
+
+        } else if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='SELL') {
+            thirdLegPl.push(-(Math.max(strikes[2]-parseFloat(resultsData[item].CALL[0]),0)-parseFloat(resultsData[item].CALL[1]))*100)
+
+        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]=='BUY'){
+            thirdLegPl.push((Math.max(parseFloat(resultsData[item].PUT[0])-strikes[2],0)-parseFloat(resultsData[item].PUT[1]))*100)
+
+        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]=='SELL'){
+            thirdLegPl.push(-(Math.max(parseFloat(resultsData[item].PUT[0])-strikes[2],0)-parseFloat(resultsData[item].PUT[1]))*100)
+        }    
+    }
+
+    for (let item in resultsData) {
+        
+        if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='BUY') {
+            fourthLegPl.push((Math.max(strikes[3]-parseFloat(resultsData[item].CALL[0]),0)-parseFloat(resultsData[item].CALL[1]))*100)
+
+        } else if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='SELL') {
+            fourthLegPl.push(-(Math.max(strikes[3]-parseFloat(resultsData[item].CALL[0]),0)-parseFloat(resultsData[item].CALL[1]))*100)
+
+        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]=='BUY'){
+            fourthLegPl.push((Math.max(parseFloat(resultsData[item].PUT[0])-strikes[3],0)-parseFloat(resultsData[item].PUT[1]))*100)
+
+        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]=='SELL'){
+            fourthLegPl.push(-(Math.max(parseFloat(resultsData[item].PUT[0])-strikes[3],0)-parseFloat(resultsData[item].PUT[1]))*100)
+        }    
+    }
+
+    for (let item in resultsData) {
+        
+        if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='BUY') {
+            infiniteCheck.push((Math.max(highestStrike-parseFloat(resultsData[item].CALL[0]),0)-parseFloat(resultsData[item].CALL[1]))*100)
+
+        } else if (Object.keys(resultsData[item])=='CALL' && resultsData[item].CALL[2]=='SELL') {
+            infiniteCheck.push(-(Math.max(highestStrike-parseFloat(resultsData[item].CALL[0]),0)-parseFloat(resultsData[item].CALL[1]))*100)
+
+        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]=='BUY'){
+            infiniteCheck.push((Math.max(parseFloat(resultsData[item].PUT[0])-highestStrike,0)-parseFloat(resultsData[item].PUT[1]))*100)
+
+        } else if (Object.keys(resultsData[item])=='PUT' && resultsData[item].PUT[2]=='SELL'){
+            infiniteCheck.push(-(Math.max(parseFloat(resultsData[item].PUT[0])-highestStrike,0)-parseFloat(resultsData[item].PUT[1]))*100)
+        }    
+    }
+
+    }
+
+
+}
+
 
 
     useEffect(() => {
         calculateResults()
     }, [resultsData])
 }
+
 
 
 
